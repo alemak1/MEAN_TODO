15060d1d888d20abfda20ace09c9d23b4f19d688
yongrenwang <czie.alexmak@gmail.com>
2017-02-16 01:05:06 +0800

    Bugfix

--- public/scripts/services/data.js
+++ public/scripts/services/data.js
@@ -11,24 +11,24 @@ angular.module('todoListApp')
   };
   
   this.saveTodos = function(todos) {
-  	var queue = [];
-  	todos.forEach(function(todo){
-  		var request;
-  		if(!todo._id){
-  			request =  $http.post('/api/todos',todo);
-  		} else {
-  			request = $http.put('/api/todos' + todo._id, todo).then(function(result){
-  				todo = result.data.todo;
-  				return todo;
-  			});
-  		}
-  		
-  		queue.push(request);
-  	});
+	  	var queue = [];
+	  	todos.forEach(function(todo){
+	  		var request;
+	  		if(!todo._id){
+	  			request =  $http.post('/api/todos',todo);
+	  		} else {
+	  			request = $http.put('/api/todos/' + todo._id, todo).then(function(result){
+	  				todo = result.data.todo;
+	  				return todo;
+	  			});
+	  		}
 
-  	return $q.all(queue).then(function(results){
-  		console.log("I saved " + todos.length + " todos!");
-  	});
+	  		queue.push(request);
+	  	});
+
+	  	return $q.all(queue).then(function(results){
+	  		console.log("I saved " + todos.length + " todos!");
+	  	});
   };
   
 });
