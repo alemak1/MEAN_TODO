f5d35f1227d77907a9b058654b6db2e6ec8098d7
yongrenwang <czie.alexmak@gmail.com>
2017-02-15 17:27:39 +0800

    Instantiated an express.Router() object to house the namespace for urls for requests made to the api namespace

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ var app = express();
 
 app.use('/',express.static('public'));
 
+var router = express.Router();
+
+router.get('/todos',function(req,res){
+	res.json({todos:[]});
+});
+
+// TODO: Add POST route to create new entries
+
+// TODO: Add PUT route to update existing entries
+
+// TODO: Add a DELETE route to delete entries
+
+app.use('/api', router);
+
 app.listen(3000, function(){
 	console.log("The server is running on port 3000!");
 });
