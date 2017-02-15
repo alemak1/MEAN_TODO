0ea22364ee1808ca490d16db41226302b214a84e
yongrenwang <czie.alexmak@gmail.com>
2017-02-16 00:11:55 +0800

    Added a post route for the '/todos' url

--- src/app.js
+++ src/app.js
@@ -4,16 +4,18 @@
 
 var express = require('express');
 var router = require('./api');
+var parser = require('body-parser');
 
 var app = express();
 
 app.use('/',express.static('public'));
+app.use(parser.json());
 
 require('./database');
 require('./seed');
 app.use('/api', router);
 
 
-app.listen(3000, function(){
-	console.log("The server is running on port 3000!");
+app.listen(5000, function(){
+	console.log("The server is running on port 5000!");
 });
